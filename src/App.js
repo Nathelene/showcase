cf7c242dcc5ecd1a007b9ec219be1e36d0ec8e6e
Natalie Baker <nathelene76@gmail.com>
2023-07-31 13:40:02 -0600

    add api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Nav from './components/Nav'
+import Card from './components/Nav'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+const [quote, setQuote] = useState("")
+const [allQuotes, setAllQuotes] = useState([])
+
+useEffect(() => {
+fetch("https://meowfacts.herokuapp.com/?count=20")
+  .then(res => res.json())
+  .then(data => setAllQuotes(data))
+
+},[])
+
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <p>Card</p>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+
+
+export default function Nav() {
+
+    return(
+
+        <h1>Cat App</h1>
+    )
+}
