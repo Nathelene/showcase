9f24dcf830747761fe2b07ab65cae0e69125628d
Natalie Baker <nathelene76@gmail.com>
2023-08-02 16:09:27 -0600

    add responsiveness to saved and home page

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,14 +8,12 @@ export default function Card({factText, toggleSavedFacts,saved,gif,loading}) {
             
         {loading? <p className="loading-message">loading img...</p> : <img className="cat-gif" src={gif}/>}
         <div className="fact-card">
-           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+           {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" /> */}
              <p>{factText}</p>
         </div>
         <button onClick={toggleSavedFacts} className="save-button"> 
              <span class="material-symbols-outlined">{saved}</span>
             </button>
         </div>
-
-    
     )
 }
