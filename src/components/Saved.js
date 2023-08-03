994bce1e1827403c0289cd3a20d807de59d59ba7
Natalie Baker <nathelene76@gmail.com>
2023-08-03 14:36:57 -0600

    clean up code

--- src/components/Saved.js
+++ src/components/Saved.js
@@ -5,32 +5,34 @@ import './Saved.css'
 
 export default function Saved({savedFacts,deleteSaved,error}) {
 
-            const savedArray = savedFacts.map((fact, index) => {
-                return (
-                <div className="save-card-container">
-                  <div className="saved-card" key={index}>
-                    <p>{fact}</p>
-                  </div>
-                  <button className="delete-button" onClick={() => deleteSaved(index)}><span class="material-symbols-outlined">delete</span></button>
-                  </div>
-                );
-              });
+  const savedArray = savedFacts.map((fact, index) => {
+      return (
+          <div className="save-card-container">
+            <div className="saved-card" key={index}>
+              <p>{fact}</p>
+            </div>
+            <button className="delete-button" onClick={() => deleteSaved(index)}>
+              <span class="material-symbols-outlined">delete</span>
+            </button>
+          </div>
+        )
+     });
         
-    
     return (
         <div>
-          {error? <h1 className="error-message">{error}</h1> :
-           <div className="saved-facts-container">
+          { error ? <h1 className="error-message">{error}</h1> :
+          <div className="saved-facts-container">
             <h3 className="saved-title">{savedArray.length} saved facts</h3>
             <NavLink to='/'>
-            <button className="back-button"><span class="material-symbols-outlined">arrow_back</span></button>
+              <button className="back-button">
+                <span class="material-symbols-outlined">arrow_back</span>
+              </button>
             </NavLink>
-            {savedArray}
-            </div>
-          }
+              {savedArray}
+          </div> }
         </div>
-    )
-}
+     )
+  };
 
 Saved.propTypes = {
   error: PropTypes.string.isRequired,
