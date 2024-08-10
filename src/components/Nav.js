008b158e3962baa7834a7c7bc2e2864302eb8b6f
Natalie Baker <nathelene76@gmail.com>
2024-08-10 12:59:52 -0600

    edit nav css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,10 +7,12 @@ const Nav = () => {
     return(
         <nav className="nav">
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-            <h1 className="header">Feline Facts</h1>
+            <NavLink to='/' style={{textDecoration:'none'}}>
+                <h1 className="header">Feline Facts</h1>
+            </NavLink>
             <NavLink to="/addFact">
                 <button className="add-fact-button">
-                  ADD FACT
+                  <span className="material-symbols-outlined">add</span>
                 </button>
             </NavLink>
             <NavLink to="/saved">
